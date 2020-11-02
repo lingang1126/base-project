21371350eb931467ea7f6516627505cf8fa3932e
lingang1126 <1126740450@qq.com>
2020-11-02 18:09:27 +0800

    Add files via upload

---
+++ poker/src/main/java/com/out/poker/specification/TaskSpec.java
@@ -0,0 +1,29 @@
+package com.out.poker.specification;
+
+import com.out.poker.common.exception.LogicException;
+
+/**
+ * PackageName: com.gugu.business.core.specification.insuranceCase
+ * FileName: CasePec.java
+ * Description: TODO 案件业务规则
+ * Copyright: Copyright (c)2020
+ * Company: 估估网络科技有限公司
+ *
+ * @author zhaoqilong@gugu.com
+ * @version 1.0, 2020-03-19
+ */
+public class TaskSpec {
+
+    /**
+     * 对象校验
+     *
+     * @param obj
+     */
+    public static void assertHanding(Object obj) {
+        if (obj == null) {
+            throw new LogicException("任务不存在");
+        }
+
+    }
+
+}
